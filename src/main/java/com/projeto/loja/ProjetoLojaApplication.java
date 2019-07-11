afc3bfaf89a6a260d6b120986d3a08cceda318b4
Joao Alvarez <jratb27@hotmail.com>
2019-07-10 22:44:14 -0300

    init corrigido e rest testado

--- src/main/java/com/projeto/loja/ProjetoLojaApplication.java
+++ src/main/java/com/projeto/loja/ProjetoLojaApplication.java
@@ -1,9 +1,13 @@
 package com.projeto.loja;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.projeto.loja.resources")
 public class ProjetoLojaApplication {
 
 	public static void main(String[] args) {

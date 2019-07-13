94654b75541f234efca39c200148724d81851c52
Joao Alvarez <jratb27@hotmail.com>
2019-07-13 17:22:26 -0300

    Criacao das exceptions personalizadas - Ajustes no Endpoint categoria/id

--- src/main/java/com/projeto/loja/services/CategoriaService.java
+++ src/main/java/com/projeto/loja/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.projeto.loja.domain.Categoria;
 import com.projeto.loja.repositories.CategoriaRepository;
+import com.projeto.loja.services.execptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }

1f15ff4df5bde31f3bf06e3f4a807ce7e78eea48
Joao Alvarez <jratb27@hotmail.com>
2019-07-14 18:53:35 -0300

    Criacao de repositorios e entidades cliente, tipoCliente(Enum), cliente e enderecos

---
+++ src/main/java/com/projeto/loja/repositories/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.projeto.loja.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projeto.loja.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco,Integer>{}

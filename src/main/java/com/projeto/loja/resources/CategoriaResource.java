10278d784f8676f34e9689619067f4af2bd08da3
Joao Alvarez <jratb27@hotmail.com>
2019-07-14 23:00:09 -0300

    criado method de listar todas as categorias(Utilizando DTO)

--- src/main/java/com/projeto/loja/resources/CategoriaResource.java
+++ src/main/java/com/projeto/loja/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.projeto.loja.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,14 +14,31 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projeto.loja.domain.Categoria;
+import com.projeto.loja.dto.CategoriaDTO;
 import com.projeto.loja.services.CategoriaService;
 
 @RestController
-@RequestMapping(value="/categoria")
+@RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@Autowired
 	private CategoriaService service;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> listCategoria = service.findAll();
+		List<CategoriaDTO> listDto = listCategoria
+				.stream().map(obj -> new CategoriaDTO(obj))
+				.collect(Collectors.toList()); 
+		/*
+		 * DTO == ADAPTER
+		 * Steam.map serve para percorrer toda a lista
+		*e para cada iten da lista fazer o new CategoriaDTO
+		* O .collect(Collectors.toList()); serve para converter uma lista para outra lista.
+		*/
+		
+		return ResponseEntity.ok().body(listDto);
+	}
 	
 	@RequestMapping(value="/{id}",  method=RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {

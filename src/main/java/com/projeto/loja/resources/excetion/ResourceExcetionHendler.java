2c4103978c64dcd67867f221881cf4d545c89ccf
Joao Alvarez <jratb27@hotmail.com>
2019-07-15 00:23:26 -0300

    Personalizado o retorno da validacao

--- src/main/java/com/projeto/loja/resources/excetion/ResourceExcetionHendler.java
+++ src/main/java/com/projeto/loja/resources/excetion/ResourceExcetionHendler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,6 +24,16 @@ public class ResourceExcetionHendler {
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandardError> DataIntegrity(DataIntegrityException e, HttpServletRequest request){
 		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value() ,e.getMessage(),System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		ValidationError error = new ValidationError(HttpStatus.BAD_REQUEST.value() ,e.getMessage(),System.currentTimeMillis());
+		
+		for (FieldError err : e.getBindingResult().getFieldErrors()) {
+			error.addError(err.getField(), err.getDefaultMessage());
+		}
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 }

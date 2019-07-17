3bf2a873e51208587c3c18ff44d3ea98726fb56d
Joao Alvarez <jratb27@hotmail.com>
2019-07-17 18:02:00 -0300

    inseridos para Cliente metodos put, get e delete

--- src/main/java/com/projeto/loja/resources/excetion/ResourceExcetionHendler.java
+++ src/main/java/com/projeto/loja/resources/excetion/ResourceExcetionHendler.java
@@ -17,19 +17,19 @@ public class ResourceExcetionHendler {
 
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
-		StandardError error = new StandardError(HttpStatus.NOT_FOUND.value() ,e.getMessage(),System.currentTimeMillis());
+		StandardError error = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
 	
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandardError> DataIntegrity(DataIntegrityException e, HttpServletRequest request){
-		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value() ,e.getMessage(),System.currentTimeMillis());
+		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
-		ValidationError error = new ValidationError(HttpStatus.BAD_REQUEST.value() ,e.getMessage(),System.currentTimeMillis());
+		ValidationError error = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
 		
 		for (FieldError err : e.getBindingResult().getFieldErrors()) {
 			error.addError(err.getField(), err.getDefaultMessage());
--- src/main/java/com/projeto/loja/services/ClienteService.java
+++ src/main/java/com/projeto/loja/services/ClienteService.java
@@ -1,23 +1,67 @@
 package com.projeto.loja.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.projeto.loja.domain.Cliente;
+import com.projeto.loja.dto.ClienteDTO;
 import com.projeto.loja.repositories.ClienteRepository;
+import com.projeto.loja.services.execptions.DataIntegrityException;
 import com.projeto.loja.services.execptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
 
 	@Autowired
-	private ClienteRepository repo;
+	private ClienteRepository repositorio;
 
 	public Cliente find(Integer id) {
-		Optional<Cliente> obj = repo.findById(id);
+		Optional<Cliente> obj = repositorio.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
+
+	public Cliente update(Cliente obj) {
+		/* Caso nao encontre o id do obj ele retorna uma excecao */
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repositorio.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		/* Caso nao encontre o id do obj ele retorna uma excecao */
+		find(id);
+		try {
+			repositorio.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel remover um Cliente que há entidade relacionadas");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repositorio.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		// ditection = asc or desc;
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repositorio.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	 
+	 private void updateData(Cliente newObj, Cliente obj) {
+		 newObj.setNome(obj.getNome());
+		 newObj.setEmail(obj.getEmail());
+	 }
+	 
 }
